4df87cb95696b75e877c5abb07de9b5edd5b0779
David de Lusenet <david.delusenet@random.studio>
2018-09-04 15:42:54 +0200

    Added some base styling

---
+++ src/app/styles/base.js
@@ -0,0 +1,23 @@
+import { injectGlobal } from 'styled-components';
+import reset from 'styled-reset';
+
+// Consts
+import { BLACK, WHITE } from '../../utils/consts';
+
+export default () => injectGlobal`
+  ${reset}
+
+  * {
+    box-sizing: border-box;
+  }
+  
+  html {
+    background-color: ${BLACK};
+    color: ${WHITE};
+  }
+  
+  body {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app/App';
+import initBaseStyles from './app/styles/base';
 
+// Initialize the base styles and render the application
+initBaseStyles();
 ReactDOM.render(<App />, document.getElementById('root'));
