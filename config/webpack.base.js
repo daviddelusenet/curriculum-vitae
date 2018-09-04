0070285025dfed6df72bb9e0a2c1ba86a942d317
David de Lusenet <david.delusenet@random.studio>
2018-09-04 14:25:25 +0200

    Added first working setup of Hello World

---
+++ config/webpack.base.js
@@ -0,0 +1,42 @@
+const { resolve } = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { distPath, publicPath } = require('./paths');
+
+module.exports = () => ({
+  entry: {
+    'curriculum-vitae': ['@babel/polyfill', resolve(__dirname, '../src/index.js')],
+  },
+  output: {
+    path: distPath,
+    filename: '[name].[hash].min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin([distPath], {
+      allowExternal: true,
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: publicPath,
+        to: distPath,
+      },
+    ]),
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, '../src/templates/index.html'),
+    }),
+  ],
+});
---
+++ config/webpack.dev.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+const { resolve } = require('path');
+const merge = require('webpack-merge');
+const baseConfig = require('./webpack.base');
+
+module.exports = () => (
+  merge(baseConfig(), {
+    devtool: 'inline-source-map',
+    devServer: {
+      contentBase: resolve(__dirname, '../dist'),
+      historyApiFallback: true,
+      hot: true,
+      open: true,
+      publicPath: '/',
+    },
+    plugins: [
+      new webpack.NamedModulesPlugin(),
+    ],
+  })
+);
