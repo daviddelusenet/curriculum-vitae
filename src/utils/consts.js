652601b41b598ef10d8a600637b6a468728db98d
David de Lusenet <david.delusenet@random.studio>
2018-09-05 10:24:47 +0200

    Added some Google fonts

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,6 +1,9 @@
 // Color variables
 // Color names by http://chir.ag/projects/name-that-color/
+export const ALABASTER = '#fafafa';
 export const BLACK = '#000';
 export const WHITE = '#fff';
 
 // Font-families
+export const OPEN_SANS = "'Open Sans', sans-serif;";
+export const SOURCE_SERIF_PRO = "'Source Serif Pro', serif";
