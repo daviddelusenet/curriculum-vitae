8882975b2cea12c48ab168c04f97479c2e5137fe
David de Lusenet <david@lusenet.nl>
2019-11-28 20:39:15 +0100

    Refactor DevelopmentSkills to make use of ListItem component + update content

--- src/app/components/DevelopmentSkills/DevelopmentSkills.js
+++ src/app/components/DevelopmentSkills/DevelopmentSkills.js
@@ -1,41 +1,61 @@
 import List from '../List/List';
+import ListItem from '../ListItem/ListItem';
 import Paragraph from '../Paragraph/Paragraph';
 import React from 'react';
 import Title from '../Title/Title';
+
+const currentTools = [
+    'HTML5',
+    'CSS3/SASS/React CSS Modules',
+    'TypeScript/JavaScript ES6',
+    'React',
+    'Redux',
+    'Node.js/Express',
+    'Apollo GraphQL',
+    'Storybook',
+    'Styled Components',
+    'GSAP',
+    'Webpack/Babel',
+    'Git',
+    'ESLint/Stylelint',
+];
+
+const previousTools = [
+    'PixiJS',
+    'Backbone.js',
+    'jQuery',
+    'PHP',
+    'MySQL',
+    'Twig',
+    'Smarty',
+    'Twitter Bootstrap',
+    'Zurb Foundation',
+    'Gulp',
+    'Grunt',
+];
 
 const DevelopmentSkills = () => (
     <>
         <Title>
             {'Development skills'}
         </Title>
-        <List listItems={[
-            'HTML5',
-            'CSS3/SASS/React CSS Modules',
-            'JavaScript ES6',
-            'React',
-            'Redux',
-            'Styled Components',
-            'GSAP',
-            'Webpack',
-            'Git',
-        ]}
-        />
+        <List>
+            {currentTools.map((tool) => (
+                <ListItem key={tool}>
+                    {tool}
+                </ListItem>
+            ))}
+        </List>
         <Paragraph>
             {'Tools I\'ve worked with in the past:'}
         </Paragraph>
-        <List listItems={[
-            'Backbone.js',
-            'jQuery',
-            'PHP',
-            'MySQL',
-            'Twig',
-            'Smarty',
-            'Twitter Bootstrap',
-            'Zurb Foundation',
-            'Gulp',
-            'Grunt',
-        ]}
-        />
+        <List>
+            {previousTools.map((tool) => (
+                <ListItem key={tool}>
+                    {tool}
+                </ListItem>
+            ))}
+        </List>
     </>
 );
 
